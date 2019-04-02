568150d5d6178faf1cb75843d43559f5780666e8
Zach Overhulser <zoverhulser@gmail.com>
2019-04-02 18:40:44 +0000

    Specifies a better state structure.

--- instagram/src/components/CommentSection/CommentInput.js
+++ instagram/src/components/CommentSection/CommentInput.js
@@ -1,22 +1,37 @@
 import React, {useState} from 'react';
 
-const CommentInput = ({comments}) => {
-	const [state, setState] = useState(comments);
+const CommentInput = ({addComment}) => {
+	const [state, setState] = useState({
+		text: "",
+		id: (new Date()).getTime(),
+		username: "zoverlvx"
+	});
 
-	function addComment(e) {
+	function handleSubmit(e) {
 		e.preventDefault();
-		const newComment = {
-			id: (new Date).getTime(),
+		addComment({
 			username: "zoverlvx",
-			text: e.target.value
-		};
-		setState(state.concat(newComment))
+			id: (new Date()).getTime(),
+			text: state.text
+		});
+		setState({text: ""});
 	}
-  return (
-    <form onSubmit={addComment}>
-      <input type="text" placeholder="Add comment... " />
-    </form>
-  );
+
+	function handleChange(e) {
+		setState({text: e.target.value});
+	}
+	
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<input 
+				type="text" 
+				placeholder="Add comment... " 
+				value={state.text}
+				onChange={handleChange}
+			/>
+		</form>
+	);
 };
 
 export default CommentInput;
