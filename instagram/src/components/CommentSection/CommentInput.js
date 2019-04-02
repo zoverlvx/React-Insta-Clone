0b1c21c0c02b4ecf2873fc2bb30a4f416b72ad31
Zach Overhulser <zoverhulser@gmail.com>
2019-04-02 01:45:25 +0000

    Implements React hooks

--- instagram/src/components/CommentSection/CommentInput.js
+++ instagram/src/components/CommentSection/CommentInput.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const CommentInput = props => {
+const CommentInput = ({comments}) => {
+	const [state, setState] = useState(comments);
+
+	function addComment(e) {
+		e.preventDefault();
+		const newComment = {
+			id: (new Date).getTime(),
+			username: "zoverlvx",
+			text: e.target.value
+		};
+		setState(state.concat(newComment))
+	}
   return (
-    <form>
+    <form onSubmit={addComment}>
       <input type="text" placeholder="Add comment... " />
     </form>
   );
