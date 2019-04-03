3cd1c9cc8cb7bb1f69e52308eb05cdad48a325fb
Zach Overhulser <zoverhulser@gmail.com>
2019-04-03 22:06:38 +0000

    Renders login page.

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,42 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
-import dummyData from './dummy-data';
-import PostsContainer from './components/PostsContainer/PostsContainer';
-import SearchBar from './components/SearchBar/SearchBarContainer';
+import PostsPage from './components/PostsPage';
+import Login from "./components/Login/Login";
+
+
 
 function App() {
-	const [state, setState] = useState({
-		posts: [],
-		filteredPosts: []
-	});
-
-	function componentDidMount () {
-	    useEffect(() => setState({posts: dummyData, filteredPosts: []}), [])
-	    return null;
-	}
-
-	componentDidMount();
-
-	function searchPostsHandler(e) {
-		console.log("value from search bar", e.target.value)
-    	const posts = state.posts.filter(p => {
-      		if (p.username.includes(e.target.value)) {
-        		return p;
-      		}
-    	});
-    	setState({ filteredPosts: posts });
-  }
-
+	
 	return (
 		<div className="App">
-			<SearchBar searchPosts={searchPostsHandler}/>
-			<PostsContainer 
-				posts={
-					state.filteredPosts.length > 0
-					? state.filteredPosts
-					: state.posts
-				} 
-			/>
+			<Login />
 		</div>
 	)
 }
