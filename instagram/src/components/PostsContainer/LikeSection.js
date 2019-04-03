8389e6b492e59b354637bd688c2336c7873f9ed6
Zach Overhulser <zoverhulser@gmail.com>
2019-04-03 22:02:35 +0000

    Fixes CSS and moves attribute to correct node.

--- instagram/src/components/PostsContainer/LikeSection.js
+++ instagram/src/components/PostsContainer/LikeSection.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import "react-fontawesome";
 
 const LikeSection = props => {
   return [
     <div
       className="like-section"
       key="likes-icons-container"
-      onClick={props.incrementLike}
     >
-      <div className="like-section-wrapper">
-        <i className="far fa-heart" />
+      <div className="like-section-wrapper"
+          onClick={props.incrementLike}
+	  >
+        <i className="fa fa-heart" />
       </div>
       <div className="like-section-wrapper">
-        <i className="far fa-comment" />
+        <i className="fa fa-comment" />
       </div>
     </div>,
     <div className="like-section" key="likes-container">
