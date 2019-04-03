ca22b9e4776ff94e0e265cbdaa8e3600cf0f95c6
Zach Overhulser <zoverhulser@gmail.com>
2019-04-03 22:00:47 +0000

    Modifies PostsPage to reflect posts rendered after authentication.

---
+++ instagram/src/components/PostsPage.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import dummyData from '../dummy-data';
+import PostsContainer from './PostsContainer/PostsContainer';
+import SearchBar from './SearchBar/SearchBarContainer';
+
+function PostsPage() {
+	const [state, setState] = useState({
+		posts: [],
+		filteredPosts: []
+	});
+
+	function componentDidMount () {
+	    useEffect(() => setState({posts: dummyData, filteredPosts: []}), [])
+	    return null;
+	}
+
+	componentDidMount();
+
+	function searchPostsHandler(e) {
+    	const posts = state.posts.filter(p => {
+      		if (p.username.includes(e.target.value)) {
+        		return p;
+      		}
+    	});
+    	setState({ 
+		    posts: state.posts,
+		    filteredPosts: posts 
+		});
+  }
+
+	return (
+		<div className="App">
+			<SearchBar searchPosts={searchPostsHandler}/>
+			<PostsContainer 
+				posts={
+					state.filteredPosts.length > 0
+					? state.filteredPosts
+					: state.posts
+				} 
+			/>
+		</div>
+	)
+}
+
+export default PostsPage;
