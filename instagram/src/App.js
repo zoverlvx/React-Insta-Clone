5add0f667683ddc077846068c9511fad31081c52
Zach Overhulser <zoverhulser@gmail.com>
2019-04-04 02:23:45 +0000

    Renders authentication user interface.

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,15 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import Authenticate from "./authenticate/Authenticate";
 import PostsPage from './components/PostsPage';
 import Login from "./components/Login/Login";
 
-
-
 function App() {
-	
 	return (
 		<div className="App">
-			<Login />
+			<Authenticate />
 		</div>
 	)
 }
