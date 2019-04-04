0f42e6d984a75c2460402ee8863481ef2e67a816
Zach Overhulser <zoverhulser@gmail.com>
2019-04-04 21:23:25 +0000

    Styles comment.

--- instagram/src/components/CommentSection/Comment.js
+++ instagram/src/components/CommentSection/Comment.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './Comment.css';
+import {UserName} from "../PostsContainer/PostHeader";
+import styled, {css} from "styled-components";
+
+export const FlexWrapper = styled.div`
+	display: flex;
+	${props => 
+		props && css`
+			line-height: 8px;
+	`};
+`;
+
+const CommentParagraph = styled.p`
+	font-weight: 400;
+	font-size: 0.8em;
+	padding: 0 5px;
+`;
 
 const Comment = ({comment: {text, username}}) => {
-  return (
-    <div className="comment-text">
-      <span className="comment">{text}</span>
-      <span className="user">-{username}</span>
-    </div>
-  );
-};
+	return (
+		<FlexWrapper>
+			<UserName>{username}</UserName>
+			<CommentParagraph>{text}</CommentParagraph>
+		</FlexWrapper>
+	);
+}
 
 Comment.propTypes = {
   comment: PropTypes.shape({
