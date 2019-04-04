4e557a9fb1af315220c78073024e6c33d18baab3
Zach Overhulser <zoverhulser@gmail.com>
2019-04-04 22:37:10 +0000

    Styles post container

--- instagram/src/components/PostsContainer/Post.js
+++ instagram/src/components/PostsContainer/Post.js
@@ -1,38 +1,64 @@
 import React, {useState} from 'react';
+import PropTypes from "prop-types";
 import CommentSection from '../CommentSection/CommentSectionContainer';
 import PostHeader from './PostHeader';
 import LikeSection from "./LikeSection";
+import styled from "styled-components";
 
 import './Posts.css';
 
-const Post = ({post}) => {
+const PostBorder = styled.div`
+	border: 1px solid #d3d3d3;
+	margin: 15px 0;
+	box-shadow: 0px 3px 15px rgba(0, 0, 0, 0.2);
+`;
+
+const PostImage = styled.img`
+	width: 100%;
+`;
+
+const Post = ({
+	post: {
+			likes, 
+			username,
+			thumbnailUrl,
+			imageUrl,
+			comments
+	}
+}) => {
 	const [state, setState] = useState({
-		likes: post.likes
+		likes: likes
 	});
 	function incrementLike() {
 	    let likes = state.likes + 1;
 	    setState({likes})
 	}
+	
 	return (
-		<div className="post-border">
+		<PostBorder>
 			<PostHeader
-				username={post.username}
-				thumbnailUrl={post.thumbnailUrl}
+				username={username}
+				thumbnailUrl={thumbnailUrl}
 			/>
-			<div className="post-image-wrapper">
-			<img
+			<PostImage
 				alt="post thumbnail"
-				className="post-image"
-				src={post.imageUrl}
+				src={imageUrl}
 			/>
-			</div>
 			<LikeSection 
 				incrementLike={incrementLike} 
 				likes={state.likes}
 			/>
-			<CommentSection comments={post.comments} />
-		</div>
+			<CommentSection comments={comments} />
+		</PostBorder>
 	);
+};
+
+Post.propTypes = {
+	post: PropTypes.shape({
+		username: PropTypes.string,
+		thumbnailUrl: PropTypes.string,
+		imageUrl: PropTypes.string
+	})
 };
 
 export default Post;
