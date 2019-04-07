e18129790109c7b4e3ab3ad5be9f7c95e0516672
Zach Overhulser <zoverhulser@gmail.com>
2019-04-07 00:30:46 +0000

    Uses new abstraction for authentication

--- instagram/src/components/Authenticate/Authenticate.js
+++ instagram/src/components/Authenticate/Authenticate.js
@@ -1,7 +1,13 @@
 import HOC from "../tools/HOC";
 import Login from "../Login/Login";
+import SignUp from "../Login/SignUp";
 import PostsPage from "../PostsContainer/PostsPage";
 
-const Authenticate = HOC(Login)(PostsPage)
+const Authenticate = HOC(SignUp)(Login)(PostsPage)({
+	ofSignUp: false,
+	ofLogin: false,
+	ofUser: "",
+	ofPassword: ""
+});
 
 export default Authenticate
