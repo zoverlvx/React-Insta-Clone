47ac64fe403b5d7c3349816f6f4c1f11667152d3
Zach Overhulser <zoverhulser@gmail.com>
2019-04-07 00:29:50 +0000

    Handles signup, login, and posts page.

--- instagram/src/components/tools/HOC.js
+++ instagram/src/components/tools/HOC.js
@@ -1,9 +1,25 @@
 import React, {useState} from 'react';
 
-const HOC = (Login) => (PostsPage) => () => {
+/*const HOC = (Login) => (PostsPage) => () => {
 	const [state, loggedIn] = useState(false);
 	if(state) return <PostsPage />;
 	return <Login loggedIn={loggedIn} />
+}
+*/
+
+const HOC = (SignUp) => (Login) => (PostsPage) => ({
+	ofSignUp,
+	ofLogin, 
+	ofUser, 
+	ofPassword
+}) => () => {
+	const [loggedIn, logIn] = useState(ofLogin);
+	const [username, setUser] = useState(ofUser);
+	const [password, setPassword] = useState(ofPassword);
+	const [signedUp, signUp] = useState(ofSignUp);
+	if (loggedIn) return <PostsPage user={username}/>
+	if(signedUp) return <Login login={logIn} />
+	return <SignUp signup={{signedUp, signUp, username, setUser, password, setPassword}}/>
 }
 
 export default HOC;
